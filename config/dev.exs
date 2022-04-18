e4b58ec6fe30f782c59d1629c963591eada8b87d
Kenan Babačić <babacic@bk.ru>
2022-04-18 15:08:05 +0200

    Update dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -4,8 +4,8 @@ use Mix.Config
 
 host = "smartexpo.bitallium.com"
 cors_proxy_host = "hubs-proxy.local"
-assets_host = "hubs-assets.local"
-link_host = "smartexpo.bitallium.com"
+assets_host = "localhost"
+link_host = ""
 
 # To run reticulum across a LAN for local testing, uncomment and change the line below to the LAN IP
 # host = cors_proxy_host = "192.168.1.27"
